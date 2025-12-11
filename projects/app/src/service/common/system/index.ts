d17a1b19c3b83304815d3f5213060b2aa94adb7f
ljc222 <ljc222@gmail.com>
2025-12-11 18:22:18 +0800

    fix:modify favicon

--- projects/app/src/service/common/system/index.ts
+++ projects/app/src/service/common/system/index.ts
@@ -150,7 +150,12 @@ export async function initSystemConfig() {
       show_coupon: process.env.SHOW_COUPON === 'true',
       show_discount_coupon: process.env.SHOW_DISCOUNT_COUPON === 'true',
       show_dataset_enhance: licenseData?.functions?.datasetEnhance,
-      show_batch_eval: licenseData?.functions?.batchEval
+      show_batch_eval: licenseData?.functions?.batchEval,
+      systemTitle: 'MetaAI',
+      favicon: '/icon/logo.svg',
+      concatMd: '',
+      docUrl: '',
+      show_git: false
     },
     systemEnv: {
       ...fileRes.systemEnv,
